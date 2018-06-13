e6d6b2171ef33649d08e8d228067221cdb4f56a9
Erick Kinnee <erick@kinnee.net>
2018-06-13 15:12:43 -0500

    Initial

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Management.Automation;
+
+namespace PSNoWindow
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (PowerShell ps = PowerShell.Create())
+            {
+
+                Collection<PSObject> results = ps.AddScript("Get-Process | Sort-Object WorkingSet | Select-Object ProcessName, WorkingSet").Invoke();
+                /*
+                for (int i = 0; i < results.Count; i++)
+                {
+                    Console.WriteLine(results[i].Properties["ProcessName"]);
+                    Console.WriteLine(results[i].Properties["WorkingSet"]);
+                }
+                */
+                //Console.ReadKey();
+            }
+        }
+    }
+}
+
